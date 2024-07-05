90eb8079f2793321ae0bb970cd03249a7cccf757
mpfarias <fariasmauricio11@outlook.com>
2024-07-05 17:52:07 -0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({ children, className, ...props }: ButtonProps) {
     <button
       {...props}
       className={twMerge(
-        "rounded-md border-2 border-black bg-transparent px-2 py-1 font-bold hover:ring-2 hover:ring-black focus:ring-2 focus:ring-black",
+        "rounded-md bg-transparent px-2 py-1 font-bold hover:ring-4 focus:ring-4",
         className,
       )}
     >
