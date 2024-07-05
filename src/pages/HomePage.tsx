7e9558a1bd7dbbc4c4f9fdab64ef9374146e3a3d
mpfarias <fariasmauricio11@outlook.com>
2024-07-05 11:36:54 -0300

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,50 +1,13 @@
-import Button from "../components/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-
-const darkIcon = <FontAwesomeIcon icon={faMoon} />;
-const lightIcon = <FontAwesomeIcon icon={faSun} />;
+import HomeFooter from "../components/homePage/HomeFooter";
+import HomeHeader from "../components/homePage/HomeHeader";
+import HomeMain from "../components/homePage/HomeMain";
 
 function HomePage() {
   return (
     <div className="mx-auto grid h-screen w-[70%] gap-10 py-12">
-      <header className="flex items-center justify-center gap-6 text-center">
-        <div>
-          <h1>Projetos</h1>
-          <hr />
-        </div>
-        <ul className="flex gap-4">
-          <li>
-            <Button>
-              <span>{lightIcon}</span>
-              <span>Light</span>
-            </Button>
-          </li>
-          <li>
-            <Button>
-              <span>{darkIcon}</span>
-              <span>Dark</span>
-            </Button>
-          </li>
-        </ul>
-      </header>
-      <main>
-        <nav className="grid place-items-center">
-          <ul className="grid grid-cols-3 gap-4">
-            <li className="size-60 rounded-md bg-slate-400 transition-all duration-300 hover:scale-110"></li>
-            <li className="size-60 rounded-md bg-slate-400 transition-all duration-300 hover:scale-110"></li>
-            <li className="size-60 rounded-md bg-slate-400 transition-all duration-300 hover:scale-110"></li>
-            <li className="size-60 rounded-md bg-slate-400 transition-all duration-300 hover:scale-110"></li>
-            <li className="size-60 rounded-md bg-slate-400 transition-all duration-300 hover:scale-110"></li>
-            <li className="size-60 rounded-md bg-slate-400 transition-all duration-300 hover:scale-110"></li>
-          </ul>
-        </nav>
-      </main>
-      <footer className="text-center">
-        <em>
-          <strong>Maurício.</strong>
-        </em>
-      </footer>
+      <HomeHeader />
+      <HomeMain />
+      <HomeFooter />
     </div>
   );
 }
