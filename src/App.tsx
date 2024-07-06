332fe55af812d9727657cee2bfab6ca4be7dbd9c
mpfarias <fariasmauricio11@outlook.com>
2024-07-06 12:23:36 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SideBar from "./components/SideBar/SideBar";
+import Filter from "./pages/Filter";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    element: <SideBar />,
+    children: [
+      {
+        path: "/pages/filter",
+        element: <Filter />,
+      },
+    ],
   },
 ]);
 
