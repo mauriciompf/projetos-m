05802ddb54599ae70511691455be4874fa9c79a4
mpfarias <fariasmauricio11@outlook.com>
2024-07-06 12:23:48 -0300

    Update HomeMain.tsx

--- src/components/homePage/HomeMain.tsx
+++ src/components/homePage/HomeMain.tsx
@@ -1,16 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function HomeMain({}) {
   return (
     <main>
-      <nav className="grid place-items-center">
+      <section className="grid place-items-center">
         <ul className="grid grid-cols-3 gap-4">
           {Array.from({ length: 6 }).map((_, index) => (
             <li
               key={index}
-              className="size-60 rounded-md bg-slate-400 transition-all duration-300 hover:scale-110"
-            ></li>
+              className="size-60 cursor-pointer rounded-md bg-slate-400 transition-all duration-300 hover:scale-110"
+            >
+              <Link
+                to={"../pages/filter"}
+                className="grid size-60 place-items-center text-2xl"
+              >
+                Filter
+              </Link>
+            </li>
           ))}
         </ul>
-      </nav>
+      </section>
     </main>
   );
 }
