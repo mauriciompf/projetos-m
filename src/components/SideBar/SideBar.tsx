ebfb90f8e24c1599fee48409f5c56f80f58f2cd5
mpfarias <fariasmauricio11@outlook.com>
2024-07-07 16:08:51 -0300

    Update SideBar.tsx

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,25 +1,24 @@
 import { Outlet } from "react-router-dom";
 import ThemeButton from "../ThemeButton";
 import useThemeContext from "../../customHooks/useThemeContext";
-import toggleThemeClasses from "../../utils/toggleThemeClasses";
+import { useState } from "react";
+// import toggleThemeClasses from "../../utils/toggleThemeClasses";
 import SideBarNav from "./SideBarNav";
 import SideBarHeader from "./SideBarHeader";
 
 export default function SideBar() {
+  const [isOpenMenu, setIsOpenMenu] = useState(true);
   const { theme } = useThemeContext();
 
   return (
     <>
       <aside
-        className={toggleThemeClasses(
-          theme === "dark" && "bg-[#1e2124]",
-          "fixed h-full min-w-[300px] border-r p-4",
-        )}
+        className={`${theme === "dark" && "bg-[#1e2124]"} ${isOpenMenu ? "w-[300px]" : "w-[60px]"} fixed h-full overflow-hidden border-r p-4 transition-all duration-500`}
       >
-        <SideBarHeader />
-        <hr className="mx-auto my-6 w-60 border-2" />
-        <SideBarNav />
-        <hr className="mx-auto my-6 w-60 border-2" />
+        <SideBarHeader setIsOpenMenu={setIsOpenMenu} isOpenMenu={isOpenMenu} />
+        <hr className="mx-auto my-6 w-auto border-2" />
+        <SideBarNav setIsOpenMenu={setIsOpenMenu} isOpenMenu={isOpenMenu} />
+        <hr className="mx-auto my-6 w-auto border-2" />
         <section className="flex justify-center gap-2">
           <ThemeButton themeName="light" />
           <ThemeButton themeName="dark" />
