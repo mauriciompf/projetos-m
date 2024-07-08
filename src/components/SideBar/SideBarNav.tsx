b4bfd8143a506ef7b244d8dcccd68bd84abefc95
mpfarias <fariasmauricio11@outlook.com>
2024-07-08 10:01:29 -0300

    Update SideBarNav.tsx

--- src/components/SideBar/SideBarNav.tsx
+++ src/components/SideBar/SideBarNav.tsx
@@ -7,19 +7,38 @@ export default function SideBarNav({ isOpenMenu }: any) {
   return (
     <section className={toggleThemeClasses(isOpenMenu && "ml-5")}>
       <nav>
-        <ul className="grid gap-4 text-xl">
+        <ul className={`grid gap-y-4 text-xl`}>
           {projectList.map((listItem) => (
             <li
               key={listItem.label}
               className={toggleThemeClasses(
-                theme === "dark"
-                  ? "hover:bg-white hover:text-black focus:bg-white focus:text-black"
-                  : "hover:bg-black hover:text-white focus:bg-black focus:text-white",
-                "flex w-min cursor-pointer items-center gap-6 rounded-md py-2",
+                !isOpenMenu
+                  ? "w-min -translate-x-1"
+                  : `cursor-pointer ${
+                      theme === "dark"
+                        ? "hover:bg-white hover:text-black focus:bg-white focus:text-black"
+                        : "hover:bg-black hover:text-white focus:bg-black focus:text-white"
+                    }`,
+                "relative flex items-center gap-6 rounded-md",
               )}
             >
-              <span className="text-lg">{listItem.icon}</span>
-              <span className="hidden">{listItem.label}</span>
+              <span
+                className={`${
+                  !isOpenMenu &&
+                  ` ${
+                    theme === "dark"
+                      ? "hover:bg-white hover:text-black focus:bg-white focus:text-black"
+                      : "hover:bg-black hover:text-white focus:bg-black focus:text-white"
+                  }`
+                } cursor-pointer rounded-md p-2 text-lg`}
+              >
+                {listItem.icon}
+              </span>
+              <span
+                className={`${!isOpenMenu && "pointer-events-none opacity-0 transition-opacity"}`}
+              >
+                {listItem.label}
+              </span>
             </li>
           ))}
         </ul>
