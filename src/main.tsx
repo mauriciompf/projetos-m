da0c821181d0d50101025618178ab80d6ebf2440
mpfarias <fariasmauricio11@outlook.com>
2024-07-08 21:52:17 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ContextThemeProvider } from "../src/context/ContextThemeProvider.tsx";
+import { ContextIsOpenMenuProvider } from "./context/ContextIsOpenMenu.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ContextThemeProvider>
-      <App />
-    </ContextThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ContextIsOpenMenuProvider>
+        <ContextThemeProvider>
+          <App />
+        </ContextThemeProvider>
+      </ContextIsOpenMenuProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
