188cbbc6f90f7c50337b8a81bf11e14a4dd57970
mpfarias <fariasmauricio11@outlook.com>
2024-07-08 21:53:26 -0300

    Update Filter.tsx

--- src/pages/Filter.tsx
+++ src/pages/Filter.tsx
@@ -1,14 +1,66 @@
+import axios from "axios";
 import WrapOutlet from "../components/WrapOutlet";
+import { useQuery } from "@tanstack/react-query";
+import useThemeContext from "../customHooks/useThemeContext";
 
 const Filter = () => {
+  const { theme } = useThemeContext();
+
+  const queryUsers = async () => {
+    const { data } = await axios.get("https://dummyjson.com/users");
+    return data.users;
+  };
+  const myQuery = useQuery({
+    queryKey: ["users"],
+    queryFn: queryUsers,
+  });
+
+  const { data: usersData, isLoading, isError } = myQuery;
+
+  const getSexNameTranslated = (sex) =>
+    sex === "female" ? "Feminino" : "Masculino";
+
   return (
-    <WrapOutlet>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste vel
-        ratione unde incidunt impedit praesentium porro dolorem non accusamus
-        ipsam! Doloremque itaque nobis pariatur explicabo nostrum soluta
-        consequuntur tenetur officia.
-      </p>
+    <WrapOutlet projectName="Filter">
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error...</p>}
+      {!isLoading && !isError && usersData && <p>Error...</p> && (
+        <section className="my-10 text-lg">
+          <table className="mx-auto w-[80%] table-auto rounded-lg">
+            <thead>
+              <tr className={`${theme === "dark" && "bg-slate-700"} `}>
+                <th className="ring-1 ring-black">ID</th>
+                <th className="p-2 ring-1 ring-black">Nome</th>
+                <th className="p-2 ring-1 ring-black">Idade</th>
+                <th className="p-2 ring-1 ring-black">Sexo</th>
+                <th className="p-2 ring-1 ring-black">Email</th>
+                <th className="p-2 ring-1 ring-black">Telefone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usersData.map((user) => (
+                <tr
+                  className={`${theme === "dark" ? ": odd:bg-slate-500 even:bg-black" : "odd:bg-white even:bg-gray-200"}`}
+                  key={user.id}
+                >
+                  <td className="00 text-center ring-1 ring-black">
+                    {user.id}
+                  </td>
+                  <td className="p-2 ring-1 ring-black">{user.firstName}</td>
+                  <td className="p-2 text-center ring-1 ring-black">
+                    {user.age}
+                  </td>
+                  <td className="p-2 ring-1 ring-black">
+                    {getSexNameTranslated(user.gender)}
+                  </td>
+                  <td className="p-2 ring-1 ring-black">{user.email}</td>
+                  <td className="p-2 ring-1 ring-black">{user.phone}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
+      )}
     </WrapOutlet>
   );
 };
