565ca2fe40d4b3cf5f93796d9572f0444b64770c
mpfarias <fariasmauricio11@outlook.com>
2024-07-08 21:52:40 -0300

    Update SideBar.tsx

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import ThemeButton from "../ThemeButton";
 import useThemeContext from "../../customHooks/useThemeContext";
-import { useState } from "react";
 // import toggleThemeClasses from "../../utils/toggleThemeClasses";
 import SideBarNav from "./SideBarNav";
 import SideBarHeader from "./SideBarHeader";
-import toggleThemeClasses from "../../utils/toggleThemeClasses";
+import useIsOpenMenu from "../../customHooks/useIsOpenMenuContext";
+// import toggleThemeClasses from "../../utils/toggleThemeClasses";
 
 export default function SideBar() {
-  const [isOpenMenu, setIsOpenMenu] = useState(true);
+  // const [isOpenMenu, setIsOpenMenu] = useState(true);
+  const { isOpenMenu } = useIsOpenMenu();
   const { theme } = useThemeContext();
 
   return (
@@ -16,18 +17,17 @@ export default function SideBar() {
       <aside
         className={`${theme === "dark" && "bg-[#1e2124]"} ${isOpenMenu ? "w-[300px]" : "w-[60px]"} fixed h-full overflow-hidden border-r p-4 transition-all duration-500`}
       >
-        <SideBarHeader setIsOpenMenu={setIsOpenMenu} isOpenMenu={isOpenMenu} />
+        <SideBarHeader />
         <hr className="mx-auto my-6 w-auto border-2" />
-        <SideBarNav setIsOpenMenu={setIsOpenMenu} isOpenMenu={isOpenMenu} />
+        <SideBarNav />
         <hr className="mx-auto my-6 w-auto border-2" />
         <section className={"flex justify-center gap-2"}>
           <ThemeButton
-            className={`${!isOpenMenu && "translate-x-6 translate-y-0"} relative transition-all duration-500`}
-            isOpenMenu={isOpenMenu}
+            className={`${!isOpenMenu && "translate-x-[1.4rem] translate-y-0"} relative transition-all duration-500`}
             themeName="light"
           />
           <ThemeButton
-            className={`${!isOpenMenu && "-translate-x-6 translate-y-10"} relative transition-all duration-500`}
+            className={`${!isOpenMenu && "-translate-x-[1.4rem] translate-y-10"} relative transition-all duration-500`}
             isOpenMenu={isOpenMenu}
             themeName="dark"
           />
