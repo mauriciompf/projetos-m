5ad410c81ab9d220dfd368694623f2b7e2caa64b
mpfarias <fariasmauricio11@outlook.com>
2024-07-10 12:01:06 -0300

    Update Filter.tsx

--- src/pages/Filter.tsx
+++ src/pages/Filter.tsx
@@ -3,8 +3,12 @@ import WrapOutlet from "../components/WrapOutlet";
 import { useQuery } from "@tanstack/react-query";
 import FilterTable from "../components/Filter/FilterTable";
 import FilterSettings from "../components/Filter/FilterSettings";
+import { useState } from "react";
 
 const Filter = () => {
+  const [orderBy, setOrderBy] = useState("");
+  const [selectColumn, setSelectColumn] = useState("");
+
   const queryUsers = async () => {
     const { data } = await axios.get("https://dummyjson.com/users");
     return data.users;
@@ -17,14 +21,23 @@ const Filter = () => {
   const { data: usersData, isLoading, isError } = myQuery;
 
   return (
-    <WrapOutlet projectName="Filter">
+    <WrapOutlet projectName="Filtro">
       {isLoading && <p>Loading...</p>}
       {isError && <p>Error...</p>}
       {!isLoading && !isError && usersData && <p>Error...</p> && (
         <>
           <section className="my-10 text-lg">
-            <FilterSettings />
-            <FilterTable usersData={usersData} />
+            <FilterSettings
+              selectColumn={selectColumn}
+              setSelectColumn={setSelectColumn}
+              orderBy={orderBy}
+              setOrderBy={setOrderBy}
+            />
+            <FilterTable
+              selectColumn={selectColumn}
+              orderBy={orderBy}
+              usersData={usersData}
+            />
           </section>
         </>
       )}
