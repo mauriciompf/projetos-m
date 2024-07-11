57369f4edf1d33c5ae92777e88e5b607e3efa11d
mpfarias <fariasmauricio11@outlook.com>
2024-07-11 18:17:52 -0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,18 @@ import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  refBtn?: any;
 }
 
-export default function Button({ children, className, ...props }: ButtonProps) {
+export default function Button({
+  children,
+  className,
+  refBtn,
+  ...props
+}: ButtonProps) {
   return (
     <button
+      ref={refBtn}
       {...props}
       className={twMerge(
         "bg-transparent px-2 py-1 font-bold hover:ring-4 focus:ring-4",
