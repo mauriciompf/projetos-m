88f2ff27e175b73e581221161443999ff40a5ec5
mpfarias <fariasmauricio11@outlook.com>
2024-07-12 10:37:09 -0300

    Update FilterSettings.tsx

--- src/components/Filter/FilterSettings.tsx
+++ src/components/Filter/FilterSettings.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import { createPortal } from "react-dom";
 import Button from "../Button";
 import { tableHeaders } from "./FilterTable";
@@ -8,6 +8,7 @@ type FilterSettingsProps = {
   setOrderBy: (val: string) => void;
   selectColumn: string;
   setSelectColumn: (val: string) => void;
+  setToggleSortBy?: (val: boolean) => void;
 };
 
 export default function FilterSettings({
@@ -17,59 +18,76 @@ export default function FilterSettings({
   setSelectColumn,
 }: FilterSettingsProps) {
   const [toggleSortBy, setToggleSortBy] = useState(false);
+  const refSortByBtn = useRef<HTMLButtonElement | null>(null);
 
+  const handleToggleSortBy = () => {
+    setToggleSortBy((prev) => !prev);
+  };
+
+  return (
+    <div className="relative mx-auto mb-6 flex w-[80%] items-center gap-4">
+      <Button
+        refBtn={refSortByBtn}
+        onClick={handleToggleSortBy}
+        className="rounded-md bg-slate-300 px-4 py-2 font-bold"
+      >
+        ORGANIZAR
+      </Button>
+
+      <Button className="rounded-md bg-slate-300 px-4 py-2 font-bold">
+        FILTRO
+      </Button>
+
+      <span>Show x/y</span>
+
+      {toggleSortBy &&
+        createPortal(
+          <SortByBox
+            orderBy={orderBy}
+            setOrderBy={setOrderBy}
+            selectColumn={selectColumn}
+            setSelectColumn={setSelectColumn}
+            setToggleSortBy={setToggleSortBy}
+          />,
+          document.body,
+        )}
+    </div>
+  );
+}
+
+function SortByBox({
+  orderBy,
+  setOrderBy,
+  selectColumn,
+  setSelectColumn,
+  setToggleSortBy,
+}: FilterSettingsProps) {
   const [toggleSelectColumn, setToggleSelectColumn] = useState(false);
   const [toggleOrderBy, setToggleOrderBy] = useState(false);
   const refSortByBox = useRef<HTMLElement | null>(null);
-  const refSortByBtn = useRef<HTMLElement | null>(null);
+
+  const OrderByLabels = ["Crescente", "Decrescente", "Padrão"];
+  const handleClickOutside = useCallback((e: MouseEvent) => {
+    const refSortByBox = useRef<HTMLDivElement | null>(null);
+
+    const target = e.target as HTMLElement;
+
+    if (refSortByBox.current && !refSortByBox.current.contains(target)) {
+      setToggleSortBy!(false);
+    }
+  }, []);
 
   useEffect(() => {
-    const handleClickOutside = (e: React.MouseEvent) => {
-      const val = e.target as HTMLInputElement;
-      console.log(val);
-
-      if (!refSortByBox.current) {
-        return;
-      }
-
-      if (val !== refSortByBtn.current) {
-        if (!refSortByBox.current.contains(val)) {
-          setToggleSortBy(false);
-        }
-      }
-    };
-
-    window.addEventListener("click", (e) => handleClickOutside(e));
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      window.removeEventListener("click", (e) => handleClickOutside(e));
+      document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [refSortByBtn, refSortByBox]);
-
-  const handleToggleSortBy = () => {
-    setToggleSortBy(!toggleSortBy);
-  };
-
-  const handleToggleSelectColumn = () => {
-    setToggleSelectColumn(!toggleSelectColumn);
-  };
-
-  const handleSelectColumn = (header: string) => {
-    setSelectColumn(header);
-  };
-
-  const handleToggleOrderBy = () => {
-    setToggleOrderBy(!toggleOrderBy);
-  };
-
-  const handleOrderBy = (label: string) => {
-    setOrderBy(label);
-    setToggleOrderBy(false);
-  };
+  }, [handleClickOutside]);
 
   const removeSelectedColumn = (col: string) => {
-    tableHeaders.forEach((column) => {
-      if (col === column) setSelectColumn("");
+    tableHeaders.forEach((header) => {
+      if (col === header) setSelectColumn("");
     });
 
     OrderByLabels.forEach((label) => {
@@ -91,98 +109,93 @@ export default function FilterSettings({
     );
   }
 
-  const OrderByLabels = ["Crescente", "Decrescente", "Padrão"];
+  const handleToggleSelectColumn = () => {
+    setToggleSelectColumn(!toggleSelectColumn);
+  };
+
+  const handleSelectColumn = (header: string) => {
+    setSelectColumn(header);
+  };
+
+  const handleToggleOrderBy = () => {
+    setToggleOrderBy(!toggleOrderBy);
+  };
+
+  const handleOrderBy = (label: string) => {
+    setOrderBy(label);
+    setToggleOrderBy(false);
+  };
 
   return (
-    <div className="relative mx-auto mb-6 flex w-[80%] items-center gap-4">
-      <Button
-        refBtn={refSortByBtn}
-        onClick={handleToggleSortBy}
-        className="rounded-md bg-slate-300 px-4 py-2 font-bold"
-      >
-        ORGANIZAR
-      </Button>
-
-      <Button className="rounded-md bg-slate-300 px-4 py-2 font-bold">
-        FILTRO
-      </Button>
-
-      <span>Show x/y</span>
-
-      {toggleSortBy &&
-        createPortal(
-          <article
-            ref={refSortByBox}
-            className="fixed left-[290px] top-[180px] flex items-start gap-2 rounded-md bg-slate-300 p-4"
+    <article
+      ref={refSortByBox}
+      className="fixed left-[290px] top-[180px] flex items-start gap-2 rounded-md bg-slate-300 p-4"
+    >
+      <div>
+        {selectColumn ? (
+          <SelectedSortBy col={selectColumn} />
+        ) : (
+          <Button
+            onClick={handleToggleSelectColumn}
+            className="border border-black p-2"
           >
-            <div>
-              {selectColumn ? (
-                <SelectedSortBy col={selectColumn} />
-              ) : (
-                <Button
-                  onClick={handleToggleSelectColumn}
-                  className="border border-black p-2"
-                >
-                  Selecione uma coluna
-                </Button>
-              )}
-              {!selectColumn && toggleSelectColumn && (
-                <ul>
-                  {tableHeaders
-                    .filter(
-                      (header) =>
-                        header !== "Sexo" &&
-                        header !== "Email" &&
-                        header !== "Telefone",
-                    )
-                    .map((header) => (
-                      <li className="border border-black" key={header}>
-                        <Button
-                          onClick={() => handleSelectColumn(header)}
-                          className="w-full p-2 text-left hover:bg-black hover:text-white focus:bg-black focus:text-white"
-                        >
-                          {header}
-                        </Button>
-                      </li>
-                    ))}
-                </ul>
-              )}
-            </div>
-            <div>
-              {orderBy ? (
-                <Button
-                  onClick={handleToggleOrderBy}
-                  className="flex w-full items-center gap-2 border border-black p-2"
-                >
-                  <span className="font-bold">{orderBy}</span>
-                </Button>
-              ) : (
-                <Button
-                  onClick={handleToggleOrderBy}
-                  className="border border-black p-2"
-                >
-                  Ordernar por
-                </Button>
-              )}
-
-              {toggleOrderBy && (
-                <ul>
-                  {OrderByLabels.map((label) => (
-                    <li className="border border-black" key={label}>
-                      <Button
-                        onClick={() => handleOrderBy(label)}
-                        className="w-full p-2 text-left hover:bg-black hover:text-white focus:bg-black focus:text-white"
-                      >
-                        {label}
-                      </Button>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          </article>,
-          document.body,
+            Selecione uma coluna
+          </Button>
         )}
-    </div>
+        {!selectColumn && toggleSelectColumn && (
+          <ul>
+            {tableHeaders
+              .filter(
+                (header) =>
+                  header !== "Sexo" &&
+                  header !== "Email" &&
+                  header !== "Telefone",
+              )
+              .map((header) => (
+                <li className="border border-black" key={header}>
+                  <Button
+                    onClick={() => handleSelectColumn(header)}
+                    className="w-full p-2 text-left hover:bg-black hover:text-white focus:bg-black focus:text-white"
+                  >
+                    {header}
+                  </Button>
+                </li>
+              ))}
+          </ul>
+        )}
+      </div>
+      <div>
+        {orderBy ? (
+          <Button
+            onClick={handleToggleOrderBy}
+            className="flex w-full items-center gap-2 border border-black p-2"
+          >
+            <span className="font-bold">{orderBy}</span>
+          </Button>
+        ) : (
+          <Button
+            onClick={handleToggleOrderBy}
+            className="border border-black p-2"
+          >
+            Ordernar por
+          </Button>
+        )}
+
+        {toggleOrderBy && (
+          <ul>
+            {OrderByLabels.map((label) => (
+              <li className="border border-black" key={label}>
+                <Button
+                  onClick={() => handleOrderBy(label)}
+                  className="w-full p-2 text-left hover:bg-black hover:text-white focus:bg-black focus:text-white"
+                >
+                  {label}
+                </Button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </article>
   );
 }
