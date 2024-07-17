8d3b99d4c3380bad4901df72011814144ed72f8b
mpfarias <fariasmauricio11@outlook.com>
2024-07-17 15:43:20 -0300

    Update SortByHeader.tsx

--- src/components/Filter/sortBy/SortByHeader.tsx
+++ src/components/Filter/sortBy/SortByHeader.tsx
@@ -1,11 +1,14 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { useThemeContext } from "../../../context/ThemeContext";
 import Button from "../../Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const upIcon = <FontAwesomeIcon icon={"fa-solid fa-sort-up" as IconProp} />;
+const downIcon = <FontAwesomeIcon icon={"fa-solid fa-sort-down" as IconProp} />;
 
 type SortByHeaderProps = {
   onClick: () => void;
   isOrderByOpen?: boolean;
-  upIcon?: any;
-  downIcon?: any;
   headerLabel: string;
   isRemoveButton?: any;
   removeButton?: any;
@@ -14,8 +17,6 @@ type SortByHeaderProps = {
 export default function SortByHeader({
   onClick,
   isOrderByOpen,
-  upIcon,
-  downIcon,
   headerLabel,
   isRemoveButton = false,
   removeButton,
@@ -29,7 +30,7 @@ export default function SortByHeader({
       <span className="font-bold">{headerLabel}</span>
       <Button
         onClick={onClick}
-        className="py-0 ring-transparent hover:text-red-500 focus:text-red-500"
+        className="w-max py-0 ring-transparent hover:text-red-500 focus:text-red-500"
       >
         {removeButton}
       </Button>
@@ -37,7 +38,7 @@ export default function SortByHeader({
   ) : (
     <Button
       onClick={onClick}
-      className={`flex items-center gap-2 border p-2 ${theme === "dark" ? "border-white" : "border-black"} `}
+      className={`flex w-max items-center gap-2 border p-2 ${theme === "dark" ? "border-white" : "border-black"} `}
     >
       <span>{headerLabel}</span>
 
