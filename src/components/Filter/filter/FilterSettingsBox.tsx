3c03a8eba75be5f5a0d55bb5c65d3fa6a441d8ce
mpfarias <fariasmauricio11@outlook.com>
2024-07-18 11:16:33 -0300

    Update FilterSettingsBox.tsx

--- src/components/Filter/filter/FilterSettingsBox.tsx
+++ src/components/Filter/filter/FilterSettingsBox.tsx
@@ -1,10 +1,55 @@
 import WrapSettingsBox from "../../WrapSettingsBox";
+import ColumnSelector from "../../ColumnSelector";
+import useClickOutside from "../../../customHooks/useClickOutside";
+import { RefObject, useRef, useState } from "react";
+import HeaderControl from "../../HeaderControl";
+import ListItem from "../../ListItem";
 
-export default function FilterSettingsBox() {
+type FilterSettingsBoxProps = {
+  refFilterBtn: RefObject<HTMLButtonElement>;
+  setToggleFilter: (val: boolean) => void;
+};
+
+export default function FilterSettingsBox({
+  refFilterBtn,
+  setToggleFilter,
+}: FilterSettingsBoxProps) {
+  // HACK useSearchParams instead of useState
+  const [statusDropdown, setStatusDropdown] = useState("");
+  const [statusToggle, setStatusToggle] = useState(false);
+  const refFilterBox = useRef<HTMLElement | null>(null);
+  useClickOutside(refFilterBox, refFilterBtn, () => setToggleFilter(false));
+  const statusLabels = ["É", "Não É"];
+
+  const handleStatusToggle = () => setStatusToggle((prev) => !prev);
+
+  const handleStatusSelect = (status: string) => {
+    setStatusDropdown(status);
+  };
+
   return (
-    <WrapSettingsBox>
-      <div>col 1</div>
-      <div>col 2</div>
+    <WrapSettingsBox refElem={refFilterBox}>
+      <ColumnSelector />
+
+      <div>
+        <HeaderControl
+          headerLabel={statusDropdown || "É"}
+          onClick={handleStatusToggle}
+          isDropDownOpen={statusToggle}
+        />
+
+        {statusToggle && (
+          <ul>
+            {statusLabels.map((label) => (
+              <ListItem
+                list={label}
+                key={label}
+                handleClick={() => handleStatusSelect(label)}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
       <div>col 3</div>
     </WrapSettingsBox>
   );
