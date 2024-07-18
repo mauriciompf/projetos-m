b2021fb44d32eedb18e7997926c07db37f8290c3
mpfarias <fariasmauricio11@outlook.com>
2024-07-18 20:54:55 -0300

    Update ToggleContext.tsx

--- src/context/ToggleContext.tsx
+++ src/context/ToggleContext.tsx
@@ -4,17 +4,25 @@ import useCustomHookContext from "../customHooks/useCustomHookContext";
 // #FIXME type any
 const ToggleContext = createContext<any | null>(null);
 
-// #FIXME type any
-function ToggleContextProvider({ children }: any) {
+function ToggleContextProvider({ children }: { children: React.ReactNode }) {
   const [orderBy, setOrderBy] = useState("");
-  const [selectColumn, setSelectColumn] = useState("");
+  const [selectColumnMap, setSelectColumnMap] = useState({});
+  // const [selectColumn, setSelectColumn] = useState("");
+
+  const setSelectColumn = (key: string, column: string) => {
+    setSelectColumnMap((prev) => ({
+      ...prev,
+      [key]: column,
+    }));
+  };
 
   return (
     <ToggleContext.Provider
       value={{
         orderBy,
         setOrderBy,
-        selectColumn,
+        setSelectColumnMap,
+        selectColumnMap,
         setSelectColumn,
       }}
     >
