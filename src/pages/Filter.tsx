1f4945de88de7982577f0bf64dbc31f52743511d
mpfarias <fariasmauricio11@outlook.com>
2024-07-19 11:46:20 -0300

    Update Filter.tsx

--- src/pages/Filter.tsx
+++ src/pages/Filter.tsx
@@ -2,6 +2,7 @@ import WrapOutlet from "../components/WrapOutlet";
 import FilterTable from "../components/Filter/FilterTable";
 import FilterSettings from "../components/Filter/FilterSettings";
 import useFetch from "../customHooks/useFetch";
+import { FilterSearchProvider } from "../context/FilterSearchContext";
 
 const Filter = () => {
   const {
@@ -11,27 +12,29 @@ const Filter = () => {
   } = useFetch("https://dummyjson.com/users", "users", "users");
 
   return (
-    <WrapOutlet projectName="Filtro">
-      {/* #HACK isLoading in all query components */}
-      {isLoading && (
-        <section className="mt-32 grid place-items-center">
-          <img
-            src="/InternetSlowdown_Day.gif"
-            className="size-32 mix-blend-multiply"
-            alt="Loading..."
-          />
-        </section>
-      )}
-      {isError && <p>Error...</p>}
-      {!isLoading && !isError && usersData && <p>Error...</p> && (
-        <>
-          <section className="my-10 text-lg">
-            <FilterSettings />
-            <FilterTable usersData={usersData} />
+    <FilterSearchProvider>
+      <WrapOutlet projectName="Filtro">
+        {/* #HACK isLoading in all query components */}
+        {isLoading && (
+          <section className="mt-32 grid place-items-center">
+            <img
+              src="/InternetSlowdown_Day.gif"
+              className="size-32 mix-blend-multiply"
+              alt="Loading..."
+            />
           </section>
-        </>
-      )}
-    </WrapOutlet>
+        )}
+        {isError && <p>Error...</p>}
+        {!isLoading && !isError && usersData && <p>Error...</p> && (
+          <>
+            <section className="my-10 text-lg">
+              <FilterSettings />
+              <FilterTable usersData={usersData} />
+            </section>
+          </>
+        )}
+      </WrapOutlet>
+    </FilterSearchProvider>
   );
 };
 
