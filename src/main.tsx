186f74830f519c9bfe8aef06a744c3496782359f
mpfarias <fariasmauricio11@outlook.com>
2024-07-20 15:43:27 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeContextProvider } from "./context/ThemeContext.tsx";
 import { MenuContextProvider } from "./context/MenuContext.tsx";
-import { ToggleContextProvider } from "./context/ToggleContext.tsx";
+// import { ToggleContextProvider } from "./context/ToggleContext.tsx";
 // import { FilterSearchProvider } from "./context/FilterSearchContext.tsx";
 
 const queryClient = new QueryClient();
@@ -13,11 +13,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <MenuContextProvider>
-        <ToggleContextProvider>
-          <ThemeContextProvider>
-            <App />
-          </ThemeContextProvider>
-        </ToggleContextProvider>
+        {/* <ToggleContextProvider> */}
+        <ThemeContextProvider>
+          <App />
+        </ThemeContextProvider>
+        {/* </ToggleContextProvider> */}
       </MenuContextProvider>
     </QueryClientProvider>
   </React.StrictMode>,
