71bc58842e1f29922464f9447626395c03653d92
mpfarias <fariasmauricio11@outlook.com>
2024-07-22 10:00:51 -0300

    Update FilterSearchContext.tsx

--- src/context/FilterSearchContext.tsx
+++ src/context/FilterSearchContext.tsx
@@ -5,13 +5,18 @@ import useCustomHookContext from "../customHooks/useCustomHookContext";
 const FilterSearchContext = createContext<{
   searchParams: URLSearchParams;
   setSearchParams: (val: any) => void;
+  statusParams: URLSearchParams;
+  setStatusParams: (val: any) => void;
 } | null>(null);
 
 function FilterSearchProvider({ children }: { children: React.ReactNode }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [statusParams, setStatusParams] = useSearchParams();
 
   return (
-    <FilterSearchContext.Provider value={{ searchParams, setSearchParams }}>
+    <FilterSearchContext.Provider
+      value={{ searchParams, setSearchParams, statusParams, setStatusParams }}
+    >
       {children}
     </FilterSearchContext.Provider>
   );
