353e08362ccd768fedbfda1f167e8d4e84320b61
mpfarias <fariasmauricio11@outlook.com>
2024-07-23 18:21:39 -0300

    Update WrapSettingsBox.tsx

--- src/components/WrapSettingsBox.tsx
+++ src/components/WrapSettingsBox.tsx
@@ -6,7 +6,7 @@ export default function WrapSettingsBox({ refElem, children }: any) {
   return (
     <article
       ref={refElem}
-      className={`${theme === "dark" ? "bg-black" : "bg-slate-300"} absolute left-0 top-14 flex w-max items-baseline gap-2 rounded-md p-4`}
+      className={`${theme === "dark" ? "bg-[#25282A]" : "bg-slate-300"} absolute left-0 top-14 z-50 flex w-max items-baseline gap-2 rounded-md p-4`}
     >
       {children}
     </article>
