ce00607bb5e09c02006e6e3d5f6f510670ac9bc9
mpfarias <fariasmauricio11@outlook.com>
2024-07-24 09:43:41 -0300

    Update FilterSearchContext.tsx

--- src/context/FilterSearchContext.tsx
+++ src/context/FilterSearchContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, Dispatch, SetStateAction, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import useCustomHookContext from "../customHooks/useCustomHookContext";
 
@@ -7,15 +7,25 @@ const FilterSearchContext = createContext<{
   setSearchParams: (val: any) => void;
   statusParams: URLSearchParams;
   setStatusParams: (val: any) => void;
+  currentTableLength: number;
+  setCurrentTableLength: Dispatch<SetStateAction<number>>;
 } | null>(null);
 
 function FilterSearchProvider({ children }: { children: React.ReactNode }) {
   const [searchParams, setSearchParams] = useSearchParams();
   const [statusParams, setStatusParams] = useSearchParams();
+  const [currentTableLength, setCurrentTableLength] = useState(0);
 
   return (
     <FilterSearchContext.Provider
-      value={{ searchParams, setSearchParams, statusParams, setStatusParams }}
+      value={{
+        searchParams,
+        setSearchParams,
+        statusParams,
+        setStatusParams,
+        currentTableLength,
+        setCurrentTableLength,
+      }}
     >
       {children}
     </FilterSearchContext.Provider>
