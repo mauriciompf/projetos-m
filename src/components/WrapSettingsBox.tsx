17f16502ec543f986924010c29bb92c08ffcdcd0
mpfarias <fariasmauricio11@outlook.com>
2024-07-25 21:13:40 -0300

    Update WrapSettingsBox.tsx

--- src/components/WrapSettingsBox.tsx
+++ src/components/WrapSettingsBox.tsx
@@ -1,12 +1,26 @@
+import { twMerge } from "tailwind-merge";
 import { useThemeContext } from "../context/ThemeContext";
+import { RefObject } from "react";
 
-// #FIXME any type
-export default function WrapSettingsBox({ refElem, children }: any) {
+type WrapSettingsBoxProps = {
+  refElem: RefObject<HTMLElement>;
+  children: React.ReactNode;
+  className: string;
+};
+
+export default function WrapSettingsBox({
+  refElem,
+  children,
+  className,
+}: WrapSettingsBoxProps) {
   const { theme } = useThemeContext();
   return (
     <article
       ref={refElem}
-      className={`${theme === "dark" ? "bg-[#25282A]" : "bg-slate-300"} absolute left-0 top-14 z-50 flex w-max items-baseline gap-2 rounded-md p-4`}
+      className={twMerge(
+        `${theme === "dark" ? "bg-[#25282A]" : "bg-slate-300"} absolute left-0 top-14 z-50 flex w-max items-baseline gap-2 rounded-md p-4`,
+        className,
+      )}
     >
       {children}
     </article>
