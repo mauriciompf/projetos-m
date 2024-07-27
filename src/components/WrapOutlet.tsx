b4ccd76ac0f80298959953f8c3f565fff65c3834
mpfarias <fariasmauricio11@outlook.com>
2024-07-27 12:52:30 -0300

    Update WrapOutlet.tsx

--- src/components/WrapOutlet.tsx
+++ src/components/WrapOutlet.tsx
@@ -13,12 +13,12 @@ export default function WrapOutlet({ children, projectName }: WrapOutletProps) {
   return (
     <main
       className={toggleThemeClasses(
-        isOpenMenu ? "ml-[300px]" : "ml-[150px]",
-        "mx-auto w-[80%] p-6 py-12 transition-all duration-500 ease-in-out",
+        isOpenMenu ? "ml-[300px]" : "ml-[60px]",
+        "mx-auto transition-all duration-500 ease-in-out",
       )}
     >
       <section>
-        <Heading className="text-center tracking-wide" as={"h2"}>
+        <Heading className="pt-12 text-center tracking-wide" as={"h2"}>
           {projectName}
         </Heading>
       </section>
