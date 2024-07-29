d213fca53b51da37df7c3e83b8d5df5ed85032c3
mpfarias <fariasmauricio11@outlook.com>
2024-07-29 20:45:06 -0300

    🐛Fix: remove toggleThemeClasses function

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,7 +1,6 @@
 import Button from "./Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-import toggleThemeClasses from "../utils/toggleThemeClasses";
 import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 import { useLocation } from "react-router-dom";
@@ -33,7 +32,6 @@ export default function ThemeButton({
     const bodyClassList = document.body.classList;
     bodyClassList.remove(
       `bg-[#181a1b]`,
-      // `bg-[#282b30]`,
       "bg-white",
       "text-white",
       `text-black`,
@@ -53,11 +51,9 @@ export default function ThemeButton({
       <Button
         onClick={handleThemeNameChange}
         className={twMerge(
-          toggleThemeClasses(
-            theme === "dark" ? "bg-white text-black" : "bg-black text-white",
-            !isHomePage && isOpenMenu && "gap-1",
-            "flex select-none shadow-sm",
-          ),
+          theme === "dark" ? "bg-white text-black" : "bg-black text-white",
+          !isHomePage && isOpenMenu && "gap-0",
+          "flex select-none gap-2 shadow-sm",
           className,
         )}
         aria-label={`Change to ${themeName === "dark" ? "dark" : "light"} theme`}
