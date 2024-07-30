4ac5d2f04c038fee51270ac7d35bee7bbbba0fb5
mpfarias <fariasmauricio11@outlook.com>
2024-07-30 09:08:18 -0300

    🔨refactor: Enhance readability and add comments

--- src/customHooks/useClickOutside.ts
+++ src/customHooks/useClickOutside.ts
@@ -7,21 +7,22 @@ const useClickOutside = (
 ) => {
   const handleClickOutside = useCallback(
     (e: MouseEvent) => {
-      const target = e.target as HTMLElement;
+      const ElementTarget = e.target as HTMLElement; // Get the element that was clicked
 
+      // Check if the clicked element is outside of the ref element and not the ref button
       if (
-        ref.current &&
-        !ref.current.contains(target) &&
-        refButton.current !== target
+        ref.current && // Ensure ref element exists
+        !ref.current.contains(ElementTarget) &&
+        refButton.current !== ElementTarget
       ) {
-        callback();
+        callback(); // Call the callback function if click is outside
       }
     },
     [ref, refButton, callback],
   );
 
   useEffect(() => {
-    window.addEventListener("mousedown", handleClickOutside);
+    window.addEventListener("mousedown", handleClickOutside); // Use mousedown to handle clicks before they are processed by other elements
 
     return () => {
       window.removeEventListener("mousedown", handleClickOutside);
