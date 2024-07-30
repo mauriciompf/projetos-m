a8d27985842715f203150d76db016377a5fba43f
mpfarias <fariasmauricio11@outlook.com>
2024-07-30 09:02:57 -0300

    🔨refactor: Simplify useToggleDropDown hook

--- src/customHooks/useToggleDropDown.ts
+++ src/customHooks/useToggleDropDown.ts
@@ -1,48 +1,33 @@
 import { useState } from "react";
-
 import { useToggleContext } from "../context/ToggleContext";
 import tableHeaders from "../utils/tableHeaders";
 
 const useToggleDropDown = (key: string) => {
-  const { setSelectColumn, selectColumnMap, setOrderByParams, orderByParams } =
-    useToggleContext();
-  const [toggleSelectColumn, setToggleSelectColumn] = useState(false);
-  const [toggleOrderBy, setToggleOrderBy] = useState(false);
+  const { setSelectColumn, selectColumnMap } = useToggleContext();
+  const [selectColumnToggle, setSelectColumnToggle] = useState(false);
   const orderByLabels = ["Crescente", "Decrescente", "Padrão"];
 
-  const removeSelectedColumn = (col: string) => {
-    // Determine if col selected in dropdown is includes in tableHeaders array
+  const handleSelectColumnToggle = () => setSelectColumnToggle((prev) => !prev);
+
+  const removeSelectedColumn = (column: string) => {
+    // Check if the selected column value is included in tableHeaders or orderByLabels
     if (
-      tableHeaders.toString().toLowerCase().includes(col) ||
-      orderByLabels.toString().toLowerCase().includes(col)
+      tableHeaders.toString().toLowerCase().includes(column) ||
+      orderByLabels.toString().toLowerCase().includes(column)
     ) {
-      setSelectColumn(key, "");
+      setSelectColumn(key, ""); // Remove selected column value from params
     }
   };
 
-  const handleToggleSelectColumn = () =>
-    setToggleSelectColumn(!toggleSelectColumn);
-
   const handleSelectColumn = (header: string) =>
-    setSelectColumn(key, header.toLowerCase());
-
-  const handleToggleOrderBy = () => setToggleOrderBy(!toggleOrderBy);
-
-  const handleSelectOrderBy = (label: string) => {
-    orderByParams.set("orderby", label.toLowerCase());
-    setOrderByParams(orderByParams);
-    setToggleOrderBy(false);
-  };
+    setSelectColumn(key, header.toLowerCase()); // Set column value in params
 
   return {
+    selectColumnToggle,
+    handleSelectColumnToggle,
     removeSelectedColumn,
-    handleToggleSelectColumn,
     handleSelectColumn,
-    handleToggleOrderBy,
-    handleSelectOrderBy,
-    toggleSelectColumn,
-    toggleOrderBy,
-    selectColumn: selectColumnMap[key],
+    selectColumn: selectColumnMap[key], // Return the currently selected column value for the given key
   };
 };
 
