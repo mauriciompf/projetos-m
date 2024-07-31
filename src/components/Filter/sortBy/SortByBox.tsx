f8f28bdc8e03b17e046e598c97b6f6eeb53542ac
mpfarias <fariasmauricio11@outlook.com>
2024-07-31 18:24:49 -0300

    🔨refactor: Separete state management into a  custom hook

--- src/components/Filter/sortBy/SortByBox.tsx
+++ src/components/Filter/sortBy/SortByBox.tsx
@@ -3,32 +3,29 @@ import WrapSettingsBox from "../../WrapSettingsBox";
 import ListItem from "../../ListItem";
 import HeaderControl from "../../HeaderControl";
 import { useToggleContext } from "../../../context/ToggleContext";
-import useToggleDropDown from "../../../customHooks/useToggleDropDown";
 import useClickOutside from "../../../customHooks/useClickOutside";
 import ColumnSelector from "../../ColumnSelector";
 import toCapitalizeCase from "../../../utils/toCapitalizeCase";
 import { useFilterSearchContext } from "../../../context/FilterSearchContext";
-import Button from "../../Button";
 import ResetParams from "../../../utils/ResetParams";
+import useSortByHandlers from "../../../customHooks/useSortByHandlers";
 
 type SortByBoxProps = {
   refSortByBtn: RefObject<HTMLButtonElement>;
   setToggleSortBy: (val: boolean) => void;
 };
+
 export default function SortByBox({
   refSortByBtn,
   setToggleSortBy,
 }: SortByBoxProps) {
-  const { orderByParams } = useToggleContext();
-  const refSortByBox = useRef<HTMLElement | null>(null);
   const OrderByLabels = ["Crescente", "Decrescente", "Padrão"];
-  useClickOutside(refSortByBox, refSortByBtn, () => setToggleSortBy(false));
-
-  // HACK useSearchParams
-  const { handleToggleOrderBy, handleSelectOrderBy, toggleOrderBy } =
-    useToggleDropDown("sortby");
-
+  const refSortByBox = useRef<HTMLElement | null>(null);
+  const { orderByParams } = useToggleContext();
+  const { handleOrderByToggle, handleSelectOrderBy, orderByToggle } =
+    useSortByHandlers();
   const { searchParams } = useFilterSearchContext();
+  useClickOutside(refSortByBox, refSortByBtn, () => setToggleSortBy(false));
 
   return (
     <WrapSettingsBox
@@ -46,19 +43,19 @@ export default function SortByBox({
         <div>
           {orderByParams.has("orderby") ? (
             <HeaderControl
-              onClick={handleToggleOrderBy}
-              isDropDownOpen={toggleOrderBy}
+              onClick={handleOrderByToggle}
+              isDropDownOpen={orderByToggle}
               headerLabel={toCapitalizeCase(orderByParams.get("orderby"))}
             />
           ) : (
             <HeaderControl
-              onClick={handleToggleOrderBy}
-              isDropDownOpen={toggleOrderBy}
+              onClick={handleOrderByToggle}
+              isDropDownOpen={orderByToggle}
               headerLabel={"Ordernar por"}
             />
           )}
 
-          {toggleOrderBy && (
+          {orderByToggle && (
             <ul>
               {OrderByLabels.map((label) => (
                 <ListItem
