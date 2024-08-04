5a18fa8490361de06d68dd380e4b7446c0fe8464
mpfarias <fariasmauricio11@outlook.com>
2024-08-03 21:07:16 -0300

    💎style: Add sticky position & adjust icons

--- src/components/Filter/FilterSettings.tsx
+++ src/components/Filter/FilterSettings.tsx
@@ -3,19 +3,26 @@ import Button from "../Button";
 import SortByBox from "./sortBy/SortByBox";
 import { useThemeContext } from "../../context/ThemeContext";
 import FilterBox from "./filter/FilterBox";
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { IconProp, library } from "@fortawesome/fontawesome-svg-core";
 import {
-  faSortDown,
-  faSortUp,
-  faSquareXmark,
+  faCircleXmark,
+  faChevronDown,
 } from "@fortawesome/free-solid-svg-icons";
 import { useFilterSearchContext } from "../../context/FilterSearchContext";
 import toCapitalizeCase from "../../utils/toCapitalizeCase";
 import { tableLength } from "../../pages/Filter";
 import useFilterSettings from "../../customHooks/useFilterSettings";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const downIcon = (
+  <FontAwesomeIcon
+    width={15}
+    height={15}
+    icon={"fa-solid fa-chevron-down" as IconProp}
+  />
+);
 
 // FontAwesome library
-library.add(faSortDown, faSortUp, faSquareXmark);
+library.add(faCircleXmark, faChevronDown);
 
 export default function FilterSettings() {
   const { theme } = useThemeContext();
@@ -38,22 +45,25 @@ export default function FilterSettings() {
   const refWrapFilter = useRef<HTMLDivElement | null>(null);
   const refFilterBtn = useRef<HTMLButtonElement | null>(null);
 
+  const buttonSettingsClass = `${theme === "dark" && "bg-[#25282A] border-none"} border border-gray-300 relative select-none rounded-3xl px-2 py-2 font-bold`;
+  const onBoxClass = `${toggleSortBy && "animate-pulse"} absolute -right-0 -top-0 size-3 rounded-full bg-green-400 z-50`;
+
   return (
-    <div className="relative mb-4 flex items-center justify-between">
+    <div
+      className={`${theme === "dark" ? "bg-[#181a1b]" : "bg-white"} sticky top-0 z-10 flex items-center justify-between py-4`}
+    >
       <div className="flex items-center gap-4">
         {/* Sort By Button and Box */}
         <div ref={refWrapSortBy} className="relative">
           <Button
             refBtn={refSortByBtn}
             onClick={handleToggleSortBy}
-            className={`${theme === "dark" ? "bg-[#25282A]" : "bg-slate-300"} select-none rounded-md px-2 py-2 font-bold`}
+            className={buttonSettingsClass}
           >
-            {isSortBy && (
-              <span
-                className={`absolute -right-1 -top-1 size-3 ${toggleSortBy && "animate-pulse"} rounded-full bg-green-400`}
-              ></span>
-            )}
-            {toggleSortBy ? "📂" : "📁"} Organizar
+            {isSortBy && <span className={onBoxClass}></span>}
+            <strong>
+              {toggleSortBy ? "📂" : "📁"} Organizar {downIcon}
+            </strong>
           </Button>
 
           {toggleSortBy && (
@@ -69,14 +79,10 @@ export default function FilterSettings() {
           <Button
             refBtn={refFilterBtn}
             onClick={handleToggleFilter}
-            className={`${theme === "dark" ? "bg-[#25282A]" : "bg-slate-300"} relative select-none rounded-md px-2 py-2 font-bold`}
+            className={buttonSettingsClass}
           >
-            {isFilter && (
-              <span
-                className={`absolute -right-1 -top-1 size-3 ${toggleFilter && "animate-pulse"} rounded-full bg-green-400`}
-              ></span>
-            )}
-            🔍 Filtrar
+            {isFilter && <span className={onBoxClass}></span>}
+            <strong>🔍 Filtrar {downIcon}</strong>
           </Button>
 
           {toggleFilter && (
@@ -113,7 +119,7 @@ export default function FilterSettings() {
 
       {/* Results Info */}
       <p className="text-right">
-        <em className="opacity-50">
+        <em className="">
           Exibindo {filtedTableLength} de {tableLength} Resultados
         </em>
       </p>
