8a924b25c727f4bbeb5ce825a56d96eb2afc1ef3
mpfarias <fariasmauricio11@outlook.com>
2024-08-05 21:48:30 -0300

    🔨 refactor: Implement useElementVisibility

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -5,10 +5,12 @@ import SideBarHeader from "./SideBarHeader";
 import { useMenuContext } from "../../context/MenuContext";
 import { useThemeContext } from "../../context/ThemeContext";
 import { useLocation } from "react-router-dom";
+import { useElementVisibility } from "../../customHooks/useElementVisibility";
 
 export default function SideBar() {
   const { isOpenMenu } = useMenuContext();
   const { theme } = useThemeContext();
+  const { elementVisible } = useElementVisibility();
   const location = useLocation();
   const isHomePage = location.pathname === "/";
 
@@ -17,9 +19,9 @@ export default function SideBar() {
       <aside
         className={`${theme === "dark" && "bg-[#1e2124]"} ${isOpenMenu ? "w-[260px]" : "w-[60px]"} fixed z-50 h-full border-r border-gray-300 p-4 transition-all duration-300 ease-in-out`}
       >
-        <SideBarHeader />
+        <SideBarHeader elementVisible={elementVisible} />
         <hr className="mx-auto my-6 w-auto rounded-full border-2 border-gray-300" />
-        <SideBarNav />
+        <SideBarNav elementVisible={elementVisible} />
         <hr className="mx-auto my-6 w-auto rounded-full border-2 border-gray-300" />
         <section className={`flex justify-center ${isOpenMenu && "gap-2"} `}>
           <ThemeButton
