e5b8b7e851b42b147e7d30d29427131d79fc0439
mpfarias <fariasmauricio11@outlook.com>
2024-08-09 21:42:26 -0300

    :hammer:refactor: Simplify access key and link

--- src/components/homePage/HomeMain.tsx
+++ src/components/homePage/HomeMain.tsx
@@ -6,13 +6,13 @@ export default function HomeMain() {
     <main>
       <section className="grid place-items-center">
         <ul className={`flex flex-col gap-4 sm:flex-row`}>
-          {projectList.map((project, index) => (
+          {projectList.map((project) => (
             <li
-              key={index}
+              key={project.id}
               className="cursor-pointer rounded-2xl bg-[#4363D2] text-white shadow-2xl transition-all duration-300 hover:scale-110"
             >
               <Link
-                to={"/pages/filter"}
+                to={project.path}
                 className="grid aspect-video size-60 text-center text-2xl"
               >
                 <img
