b9eb3584dc7d2cd57d30b0296328709b8ff3f444
mpfarias <fariasmauricio11@outlook.com>
2024-08-09 21:44:33 -0300

    :hammer:refactor: Simplify project name access

--- src/pages/Filter.tsx
+++ src/pages/Filter.tsx
@@ -5,6 +5,7 @@ import useFetch from "../customHooks/useFetch";
 import { FilterSearchProvider } from "../context/FilterSearchContext";
 import { ToggleContextProvider } from "../context/ToggleContext";
 import Loading from "../components/Loading";
+import projectList from "../utils/projectList";
 
 const tableLength = 50;
 
@@ -19,7 +20,7 @@ export default function Filter() {
     <div className="selection:bg-blue-400">
       <FilterSearchProvider>
         <ToggleContextProvider>
-          <WrapOutlet projectName="Ordenação e Filtro">
+          <WrapOutlet projectName={projectList[0].label}>
             {isError && <p>Error...</p>}
 
             <section className="relative mx-auto my-0 w-[min(950px,_100%)] text-lg md:my-10">
