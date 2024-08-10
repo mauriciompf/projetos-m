4910201c69471183a0c52c1651c63b6119b7f101
mpfarias <fariasmauricio11@outlook.com>
2024-08-09 21:46:37 -0300

    :sparkles:feat: Add id and path

--- src/utils/projectList.ts
+++ src/utils/projectList.ts
@@ -1,19 +1,25 @@
 import img from "../../assets/images/image.png";
 
 type Projects = {
+  id: number;
   demoImg?: string;
   label: string;
+  path: string;
   icon: string;
 };
 
 const projectList: Projects[] = [
   {
+    id: 1,
     demoImg: img,
     label: "Ordenação e Filtro",
+    path: "/pages/filter",
     icon: "📊",
   },
   {
+    id: 2,
     label: "Galeria",
+    path: "/pages/gallery",
     icon: "🖼️",
   },
 ];
